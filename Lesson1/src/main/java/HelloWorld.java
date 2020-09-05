4a72b7daa8205af6b5b415381de06031e1f842ef
SamvelAvetisyan <70779212+SamvelAvetisyan@users.noreply.github.com>
2020-09-05 19:28:28 +0300

    Lesson1: Hello World class added

---
+++ Lesson1/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        helloWorldPrinter();
+    }
+    public static void helloWorldPrinter() {
+        System.out.println("Hello World");
+    }
+}
