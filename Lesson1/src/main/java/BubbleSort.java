dc7a04e9426398a04c8f8f6fb02f70091d168d4c
SamvelAvetisyan <70779212+SamvelAvetisyan@users.noreply.github.com>
2020-09-06 17:09:49 +0300

    Класс BubbleSort

---
+++ Lesson1/src/main/java/BubbleSort.java
@@ -0,0 +1,16 @@
+public class BubbleSort {
+    public static void bubbleSort(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    int x = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = x;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+    }
+}
